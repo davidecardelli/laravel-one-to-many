7e2e9dba0d0fd65e662b7ffeb603906bdcc92567
davidecardelli <davide.cardelli.94@gmail.com>
2023-03-13 09:37:33 +0100

    Type: add controller and views

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -4,5 +4,6 @@
     <div class="container text-center">
         <h2>Pannello di controllo</h2>
         <a href="{{ route('admin.projects.index') }}" class="btn btn-primary">Gestisci i tuoi progetti</a>
+        <a href="{{ route('admin.types.index') }}" class="btn btn-primary">Crea un nuovo tipo</a>
     </div>
 @endsection
